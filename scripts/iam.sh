3f553a649a13c2caa6e22e4102e3ca857346f333
nsubrahm-aws <nsubrahm@amazon.com>
2021-04-29 16:07:38 +0530

    RDS policy name change.

--- scripts/iam.sh
+++ scripts/iam.sh
@@ -11,7 +11,7 @@ i_setup() {
   cd ${JSON_DIR}
 
   sed -e 's/regionId/'"${REGION_ID}"'/g' -e 's/accountId/'"${ACCOUNT_ID}"'/g' -e 's/dbResourceId/'"${DB_RESOURCE_ID}"'/g' eks-saga-rds-iam-policy.json > eks-saga-rds-iam-policy.json.policy
-  POLICY_ARN=`aws iam create-policy --policy-name eks-saga-rds-policy --policy-document file://eks-saga-rds-iam-policy.json.policy --query 'Policy.Arn' --output text`
+  POLICY_ARN=`aws iam create-policy --policy-name eks-saga-rds-orche-policy --policy-document file://eks-saga-rds-iam-policy.json.policy --query 'Policy.Arn' --output text`
   echo "RDS Policy ARN: ${POLICY_ARN}"
 
   rm eks-saga-rds-iam-policy.json.policy
